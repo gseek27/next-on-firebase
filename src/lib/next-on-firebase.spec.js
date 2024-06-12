059478fd140f9d18a5f7d1241fd10acd5e301a4b
gseek27 <goalseek27@gmail.com>
2024-06-12 11:19:45 -0500

    Initial commit
    commit all

---
+++ src/lib/next-on-firebase.spec.js
@@ -0,0 +1,6 @@
+import { nextOnFirebase } from './next-on-firebase';
+describe('nextOnFirebase', () => {
+  it('should work', () => {
+    expect(nextOnFirebase()).toEqual('next-on-firebase');
+  });
+});
